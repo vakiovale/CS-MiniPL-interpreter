30de797c62d0195bbb9cc5e8c837484ef029ef76
Valtteri Pyyhtiä <valtteri.pyyhtia@gmail.com>
2018-03-18 15:04:00 +0200

    Slight refactoring

--- src/MiniPL/scanner/IScanner.cs
+++ src/MiniPL/scanner/IScanner.cs
@@ -5,11 +5,8 @@ namespace MiniPL.scanner {
   public interface IScanner { 
 
     String getSource();
-
     void setSource(String source);
-
     char readNextCharacter();
-
     bool hasNext();
 
   }
--- src/MiniPL/scanner/ITokenScanner.cs
+++ src/MiniPL/scanner/ITokenScanner.cs
@@ -1,10 +1,10 @@
 using System;
 
 namespace MiniPL.scanner {
+
   public interface ITokenScanner {
       
       String getSource();
-
       void setSource(String source);
   }
 }
