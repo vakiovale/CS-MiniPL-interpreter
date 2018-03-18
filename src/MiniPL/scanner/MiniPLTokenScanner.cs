15b304b40f6f09a9dfdd774a9a9818de0256fc61
Valtteri Pyyhtiä <valtteri.pyyhtia@gmail.com>
2018-03-18 20:43:46 +0200

    Tests for string literals and reserved keywords

--- src/MiniPL/scanner/MiniPLTokenScanner.cs
+++ src/MiniPL/scanner/MiniPLTokenScanner.cs
@@ -1,11 +1,32 @@
 using System;
+using System.Text;
+using System.Collections.Generic;
 using MiniPL.scanner;
 
 namespace MiniPL.tokens {
 
   public class MiniPLTokenScanner : TokenScanner<MiniPLTokenType> {
 
-    public MiniPLTokenScanner(IScanner characterScanner) : base(characterScanner) {}
+    private IDictionary<String, MiniPLTokenType> keywords;
+
+    public MiniPLTokenScanner(IScanner characterScanner) : base(characterScanner) {
+      initializeKeywords();
+    }
+
+    private void initializeKeywords() {
+      keywords = new Dictionary<String, MiniPLTokenType>();
+      keywords.Add("var", MiniPLTokenType.KEYWORD_VAR);
+      keywords.Add("for", MiniPLTokenType.KEYWORD_FOR);
+      keywords.Add("end", MiniPLTokenType.KEYWORD_END);
+      keywords.Add("in", MiniPLTokenType.KEYWORD_IN);
+      keywords.Add("do", MiniPLTokenType.KEYWORD_DO);
+      keywords.Add("read", MiniPLTokenType.KEYWORD_READ);
+      keywords.Add("print", MiniPLTokenType.KEYWORD_PRINT);
+      keywords.Add("assert", MiniPLTokenType.KEYWORD_ASSERT);
+      keywords.Add("int", MiniPLTokenType.TYPE_IDENTIFIER_INTEGER);
+      keywords.Add("string", MiniPLTokenType.TYPE_IDENTIFIER_STRING);
+      keywords.Add("bool", MiniPLTokenType.TYPE_IDENTIFIER_BOOL);
+    }
 
     public override Token<MiniPLTokenType> readNextToken() { 
       if(!hasNext()) {
@@ -20,51 +41,104 @@ namespace MiniPL.tokens {
     }
 
     private bool hasWhitespace() {
-      return Char.IsWhiteSpace(this.characterScanner.peek());
+      return Char.IsWhiteSpace(peek());
     }
 
     private void removeWhitespace() {
-      this.characterScanner.readNextCharacter();
+      readNextCharacter();
     }
 
-    private Token<MiniPLTokenType> readToken() {
+    private bool isLetter(char character) {
+      return Char.IsLetter(character);
+    }
+
+    private bool nextCharacterIsUnderscoreLetterOrDigit() {
+      if(!hasNext()) {
+        return false;
+      } else {
+        char peekedCharacter = peek();
+        return Char.IsLetterOrDigit(peekedCharacter) || peekedCharacter == '_'; 
+      }
+    }
+
+    private char peek() {
+      return this.characterScanner.peek();
+    }
+
+    private char readNextCharacter() {
+      return this.characterScanner.readNextCharacter();
+    }
+
+    private Token<MiniPLTokenType> createStringLiteral(String lexeme) {
+      return new Token<MiniPLTokenType>(MiniPLTokenType.STRING_LITERAL, lexeme);
+    }
+
+    private void removeWhitespaceIfExists() {
       while(hasNext() && hasWhitespace()) {
         removeWhitespace();
       }
-      char nextCharacter = this.characterScanner.readNextCharacter();
-      switch(nextCharacter) {
+    }
+
+    private bool isReservedKeyword(String content) {
+      return this.keywords.ContainsKey(content);
+    }
+
+    private Token<MiniPLTokenType> getKeywordToken(String key) {
+      return createToken(this.keywords[key]);
+    }
+
+    private Token<MiniPLTokenType> readToken() {
+      removeWhitespaceIfExists();
+      char character = readNextCharacter();
+
+      if(isLetter(character)) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.Append(character);
+        while(hasNext() && nextCharacterIsUnderscoreLetterOrDigit()) {
+          character = readNextCharacter(); 
+          stringBuilder.Append(character);
+        }
+        String token = stringBuilder.ToString();
+        if(isReservedKeyword(token)) {
+          return getKeywordToken(token);
+        } else {
+          return createStringLiteral(token);
+        }
+      }
+
+      switch(character) {
         case ';':
-          return getSingleCharacterToken(MiniPLTokenType.SEMICOLON);
+          return createToken(MiniPLTokenType.SEMICOLON);
         case '=':
-          return getSingleCharacterToken(MiniPLTokenType.EQUALITY_COMPARISON);
+          return createToken(MiniPLTokenType.EQUALITY_COMPARISON);
         case '<':
-          return getSingleCharacterToken(MiniPLTokenType.LESS_THAN_COMPARISON);
+          return createToken(MiniPLTokenType.LESS_THAN_COMPARISON);
         case '+':
-          return getSingleCharacterToken(MiniPLTokenType.PLUS);
+          return createToken(MiniPLTokenType.PLUS);
         case '-':
-          return getSingleCharacterToken(MiniPLTokenType.MINUS);
+          return createToken(MiniPLTokenType.MINUS);
         case '*':
-          return getSingleCharacterToken(MiniPLTokenType.ASTERISK);
+          return createToken(MiniPLTokenType.ASTERISK);
         case '/':
-          return getSingleCharacterToken(MiniPLTokenType.SLASH);
+          return createToken(MiniPLTokenType.SLASH);
         case '&':
-          return getSingleCharacterToken(MiniPLTokenType.LOGICAL_AND);
+          return createToken(MiniPLTokenType.LOGICAL_AND);
         case '!':
-          return getSingleCharacterToken(MiniPLTokenType.LOGICAL_NOT);
+          return createToken(MiniPLTokenType.LOGICAL_NOT);
         case '(':
-          return getSingleCharacterToken(MiniPLTokenType.LEFT_PARENTHESIS);
+          return createToken(MiniPLTokenType.LEFT_PARENTHESIS);
         case ')':
-          return getSingleCharacterToken(MiniPLTokenType.RIGHT_PARENTHESIS);
+          return createToken(MiniPLTokenType.RIGHT_PARENTHESIS);
         case '\"':
-          return getSingleCharacterToken(MiniPLTokenType.QUOTE);
+          return createToken(MiniPLTokenType.QUOTE);
         case '\\':
-          return getSingleCharacterToken(MiniPLTokenType.BACKSLASH);
+          return createToken(MiniPLTokenType.BACKSLASH);
         default:
           return new Token<MiniPLTokenType>(MiniPLTokenType.INVALID_TOKEN);
       }
     }
 
-    private Token<MiniPLTokenType> getSingleCharacterToken(MiniPLTokenType type) {
+    private Token<MiniPLTokenType> createToken(MiniPLTokenType type) {
       return new Token<MiniPLTokenType>(type);
     }
   }
--- test/MiniPL.Tests/scanner.Tests/TokenScannerTest.cs
+++ test/MiniPL.Tests/scanner.Tests/TokenScannerTest.cs
@@ -117,6 +117,72 @@ namespace MiniPL.Tests.scanner.Tests {
       Assert.Equal(MiniPLTokenType.PLUS, this.tokenScanner.readNextToken().getType());
       Assert.Equal(MiniPLTokenType.LOGICAL_AND, this.tokenScanner.readNextToken().getType());
     }
+
+    [Fact]
+    public void readMultipleDifferentSingleCharTokens() {
+      this.tokenScanner = new MiniPLTokenScanner(new Scanner("(\"!&<=\")"));
+      Assert.Equal(MiniPLTokenType.LEFT_PARENTHESIS, this.tokenScanner.readNextToken().getType());
+      Assert.Equal(MiniPLTokenType.QUOTE, this.tokenScanner.readNextToken().getType());
+      Assert.Equal(MiniPLTokenType.LOGICAL_NOT, this.tokenScanner.readNextToken().getType());
+      Assert.Equal(MiniPLTokenType.LOGICAL_AND, this.tokenScanner.readNextToken().getType());
+      Assert.Equal(MiniPLTokenType.LESS_THAN_COMPARISON, this.tokenScanner.readNextToken().getType());
+      Assert.Equal(MiniPLTokenType.EQUALITY_COMPARISON, this.tokenScanner.readNextToken().getType());
+      Assert.Equal(MiniPLTokenType.QUOTE, this.tokenScanner.readNextToken().getType());
+      Assert.Equal(MiniPLTokenType.RIGHT_PARENTHESIS, this.tokenScanner.readNextToken().getType());
+    }
+
+    [Fact]
+    public void readStringLiteralMiddleOfSpecialCharacters() {
+      this.tokenScanner = new MiniPLTokenScanner(new Scanner("(!goodVariable_1)"));
+      dynamic leftParenthesis = this.tokenScanner.readNextToken();
+      dynamic logicalNot = this.tokenScanner.readNextToken();
+      dynamic identifier = this.tokenScanner.readNextToken();
+      dynamic rightParenthesis = this.tokenScanner.readNextToken();
+      Assert.Equal(MiniPLTokenType.LEFT_PARENTHESIS, leftParenthesis.getType());
+      Assert.Equal(MiniPLTokenType.LOGICAL_NOT, logicalNot.getType());
+      Assert.Equal(MiniPLTokenType.RIGHT_PARENTHESIS, rightParenthesis.getType());
+      Assert.Equal(MiniPLTokenType.STRING_LITERAL, identifier.getType());
+      Assert.Equal("goodVariable_1", identifier.getLexeme());
+    }
+
+    [Theory]
+    [InlineData("variable_1")]
+    [InlineData("var2")]
+    [InlineData("MysticalVariable")]
+    [InlineData("A_b_123_assert")]
+    [InlineData("variable9_")]
+    public void readStringTokenTest(String source) {
+      this.tokenScanner = new MiniPLTokenScanner(new Scanner(source));
+      dynamic token = this.tokenScanner.readNextToken();
+      Assert.Equal(MiniPLTokenType.STRING_LITERAL, token.getType());
+      Assert.Equal(source, token.getLexeme());
+    }
+
+    [Fact]
+    public void readStringLiterals() {
+      this.tokenScanner = new MiniPLTokenScanner(new Scanner("token1 token2 token_3"));
+      dynamic firstToken = this.tokenScanner.readNextToken();
+      dynamic secondToken = this.tokenScanner.readNextToken();
+      dynamic thirdToken = this.tokenScanner.readNextToken();
+      Assert.Equal("token1", firstToken.getLexeme());
+      Assert.Equal("token2", secondToken.getLexeme());
+      Assert.Equal("token_3", thirdToken.getLexeme());
+    }
+
+    [Fact]
+    public void readVarKeyword() {
+      this.tokenScanner = new MiniPLTokenScanner(new Scanner("var"));
+      dynamic token = this.tokenScanner.readNextToken();
+      Assert.Equal(MiniPLTokenType.KEYWORD_VAR, token.getType());
+    }
+
+    [Fact]
+    public void readForKeyword() {
+      this.tokenScanner = new MiniPLTokenScanner(new Scanner("for"));
+      dynamic token = this.tokenScanner.readNextToken();
+      Assert.Equal(MiniPLTokenType.KEYWORD_FOR, token.getType());
+    }
+
   }
 
 }
